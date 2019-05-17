b6ed28c13b464c7312039b88771c21361c52c9e1
miaomiao1024 <1024332886@qq.com>
2019-05-17 19:27:49 +0800

    add:commit

--- src/routes/Inspection/EntranceWork/views/Index/index.jsx
+++ src/routes/Inspection/EntranceWork/views/Index/index.jsx
@@ -10,7 +10,7 @@ import axios from 'axios';
 import moment from 'moment';
 const FormItem = Form.Item;
 const FIRST_PAGE = 0;
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 6;
   
 class Homework extends Component {
   constructor(props) {
