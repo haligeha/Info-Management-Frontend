9e1608ab3a989d8837f78ee26fa7360017f585f4
miaomiao1024 <1024332886@qq.com>
2020-01-10 20:13:29 +0800

    add:rongqi

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,13 +4,17 @@
 //const target = 'http://10.112.217.199:8086';
 //const targets = 'http://10.112.12.81:8100';  // 实验室服务器-学姐
 //const target = 'http://10.112.12.81:8086';  // 实验室服务器-张宇非
+const target = 'http://10.112.217.199:30300';  // 容器API
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    proxy("/api/v1/user/", { target: "http://10.112.12.81:8086" }) // 张宇非
+    proxy("/api/", { target: target })
   );
-  app.use(
-    proxy("/api/v1/info/", { target: "http://10.112.12.81:8100" }) // 艺
-  );
+  // app.use(
+  //   proxy("/api/v1/user/", { target: "http://10.112.12.81:8086" }) // 张宇非
+  // );
+  // app.use(
+  //   proxy("/api/v1/info/", { target: "http://10.112.12.81:8100" }) // 艺
+  // );
 };
